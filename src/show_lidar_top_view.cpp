f6d40d9dbab050736d9c852189482f8789da0186
seekdestiny <jifeiqian@gmail.com>
2020-03-10 21:32:32 -0700

    complete exercise

--- src/show_lidar_top_view.cpp
+++ src/show_lidar_top_view.cpp
@@ -28,8 +28,21 @@ void showLidarTopview()
         int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
         int x = (-yw * imageSize.height / worldSize.height) + imageSize.width / 2;
 
-        cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, 0, 255), -1);
+        //cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, 0, 255), -1);
         
+        // STUDENT EXERCISE 2
+        float zw = (*it).z; // world position in m with y facing left from sensor
+        if (zw > -1.40) {
+        // EOF STUDENT EXERCISE 2            
+
+            // STUDENT EXERCISE 1
+            float val = it->x;
+            float maxVal = worldSize.height;
+            int red = min(255, (int)(255 * abs((val - maxVal) / maxVal)));
+            int green = min(255, (int)(255 * (1 - abs((val - maxVal) / maxVal))));
+            cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, green, red), -1);
+            // EOF STUDENT EXERCISE 1
+        }
         // TODO: 
         // 1. Change the color of the Lidar points such that 
         // X=0.0m corresponds to red while X=20.0m is shown as green.
